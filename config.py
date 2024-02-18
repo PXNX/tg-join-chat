50a375000a0b4153263b355800143964200fae80
nyx <loool1912@web.de>
2024-02-18 02:02:45 +0100

    fix

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
-PHONE= os.getenv("PHONE")
+PHONE = os.getenv("PHONE")
--- functions.py
+++ functions.py
@@ -8,7 +8,9 @@ from util import write_yaml, ChatInfo, read_yaml, info_from_dict
 
 
 async def list_joined(app: Client):
-    output = dict()
+    output = {}
+
+    print("listing joined")
 
     async for dialog in app.get_dialogs():
         if dialog.chat.type in (dialog.chat.type.CHANNEL, dialog.chat.type.GROUP, dialog.chat.type.SUPERGROUP):
@@ -17,7 +19,8 @@ async def list_joined(app: Client):
                 dialog.chat.title,
                 dialog.chat.username
             )
-            output.update(element.to_dict())
+            print("listing joined -")
+            output |= element.to_dict()
             info(f"Added chat: {element.to_dict()}")
             await sleep(5)
 
@@ -25,8 +28,8 @@ async def list_joined(app: Client):
 
 
 async def join_list(app: Client):
-    success = dict()
-    failure = dict()
+    success = {}
+    failure = {}
 
     for chat in info_from_dict(read_yaml("join_list")):
         info(f"Trying to join {chat.id} :: {chat.name}")
@@ -38,22 +41,22 @@ async def join_list(app: Client):
             else:
                 raise ValueError("Please provide username or invitelink.")
 
-            success.update(chat.to_dict())
+            success |= chat.to_dict()
             info(result)
         except pyrogram.errors.HashInvalid:
             message = f"Could not join {chat.id} :: {chat.name} - Maybe you are banned or invite link expired."
             error(message)
-            failure.update({id: message})
+            failure[id] = message
             pass
         except pyrogram.errors.UsernameNotOccupied:
             message = f"Could not join {chat.id} :: {chat.name} - There is no such username {chat.username}."
             error(message)
-            failure.update({id: message})
+            failure[id] = message
             pass
         except Exception as e:
             message = f"Could not join {chat.id} :: {chat.name} - {e}"
             error(message)
-            failure.update({id: message})
+            failure[id] = message
             pass
 
         await sleep(300)
